8d7342e886d4612187fb07fc59236a56f49afa22
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-06-02 18:29:15 -0500

    perf est improved

--- dev/perf/core.clj
+++ dev/perf/core.clj
@@ -6,26 +6,34 @@
    #_[pinkgorilla.ui.gorilla-plot.core :refer [list-plot]])
   (:gen-class))
 
-(defn once []
-  (let [path "/tmp/"
         ;d [1 3 5 7 9 5 4 6 9 8 3 5 6]
         ;spec (list-plot d)
         ;spec-json (json/generate-string spec)
-        spec-json (slurp "notebooks/stacked-bar.vg.json")
-        _ (println "spec: " spec-json)]
-    (println "vega -> svg performance tests..")
 
-  ;; write an SVG from a Vega spec
+(def spec-json (slurp "notebooks/stacked-bar.vg.json"))
+(println "spec: " spec-json)
+
+
+
+(defn vega->svg-str []
+  (->> spec-json
+       darkstar/vega-spec->svg))
+
+(defn vega->svg-file []
+  (let [path "/tmp/"]
     (->> spec-json
          darkstar/vega-spec->svg
          (spit (str path "vg-example.svg")))))
 
 (defn -main []
+  (println "vega -> svg performance tests..")
   ;; We'll request to send `profile` stats to `println`:
   (tufte/add-basic-println-handler! {})
-  (once) ; pre jit
+  (vega->svg-str)
+  (vega->svg-file) ; pre jit
   (profile ; Profile any `p` forms called during body execution
    {} ; Profiling options; we'll use the defaults for now
    (dotimes [_ 100]
-     (p :vega-render (once))))
+     (p :vega->svg-str (vega->svg-str))
+     (p :vega->svg-file (vega->svg-file))))
   (Thread/sleep 100))
