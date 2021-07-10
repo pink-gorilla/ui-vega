3eae9f8d63f5120a9f831194b77135b7b76118d2
CI/CD <ci@pinkgorilla.org>
2021-07-10 05:56:04 +0000

    Release 0.0.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/ui-vega "0.0.12-SNAPSHOT"
+(defproject org.pinkgorilla/ui-vega "0.0.12"
   :description "Vega renderer and a simple data-driven plotting dsl."
   :url "https://github.com/pink-gorilla/ui-vega"
   :license {:name "MIT"}
